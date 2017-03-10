4f2446f4b89204dcddd43259bf862753295780c7
島崎倫也 <acfa2656@chiba-u.jp>
2017-03-10 19:49:57 +0900

    finish book regist

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,26 @@
 class BooksController < ApplicationController
-  def new
-  end
+    
+    def create
+        @book = Book.new(book_params)
+        if @book.save
+            flash[:success] = "Complete to regist a book"
+            redirect_to @book
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+        @book = Book.new
+    end
+    
+    private
+    
+    def book_params
+        params.require(:book).permit(:title, :author)
+    end
 end
