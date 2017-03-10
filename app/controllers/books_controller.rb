3e1bad2f3256d6b3d4347a49a7904022d5059ac5
島崎倫也 <acfa2656@chiba-u.jp>
2017-03-10 21:04:28 +0900

    finish edit, update, index, destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,17 @@
 class BooksController < ApplicationController
     
+    def index
+        @books = Book.paginate(page: params[:page])
+    end
+    
+    def show
+        @book = Book.find(params[:id])
+    end
+
+    def new
+        @book = Book.new
+    end
+    
     def create
         @book = Book.new(book_params)
         if @book.save
@@ -10,12 +22,24 @@ class BooksController < ApplicationController
         end
     end
     
-    def show
+    def edit
         @book = Book.find(params[:id])
     end
-
-    def new
-        @book = Book.new
+    
+    def update
+        @book = Book.find(params[:id])
+        if @book.update_attributes(book_params)
+            flash[:success] = "information updated"
+            redirect_to @book
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        Book.find(params[:id]).destroy
+        flash[:success] = "Book deleted"
+        redirect_to books_url
     end
     
     private
